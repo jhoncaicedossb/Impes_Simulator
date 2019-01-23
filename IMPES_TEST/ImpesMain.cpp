60e258629ed9567588f038b89399c91b81634a8e
Kevin Caicedo <kevinssb@hotmail.com>
2019-01-23 18:43:57 -0500

    Testing kraken
    jdbdjfsd

---
+++ IMPES_TEST/ImpesMain.cpp
@@ -0,0 +1,72 @@
+//$Id: main.cc,v 1.21 2010-07-22 17:43:43 caju Exp $
+#ifdef HAVE_CONFIG_H
+#include <pz_config.h>
+#endif
+
+#include "pzgeoel.h"
+#include "pzgnode.h"
+#include "pzgmesh.h"
+#include "pzbiharmonic.h"
+#include "pzcmesh.h"
+#include "pzintel.h"
+#include "pzcompel.h"
+
+#include "pzfmatrix.h"
+#include "pzvec.h"
+#include "pzmanvector.h"
+#include "pzstack.h"
+
+#include "pzanalysis.h"
+#include "pzfstrmatrix.h"
+#include "pzskylstrmatrix.h"
+#include "pzstepsolver.h"
+#include "pzgeopyramid.h"
+#include "TPZGeoLinear.h"
+
+#include <TPZRefPattern.h>
+
+#include "TPZMaterial.h"
+#include "pzelasmat.h"
+#include "pzlog.h"
+
+#include "pzgengrid.h"
+
+#include <time.h>
+#include <stdio.h>
+
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <sstream>
+
+#include <set>
+#include <map>
+#include <vector>
+#include "TPZRefPatternDataBase.h"
+#include "TPZRefPatternTools.h"
+#include "TPZVTKGeoMesh.h"
+#include "TPZExtendGridDimension.h"
+
+
+
+
+#include "TPZMatLaplacian.h"
+#include "pzpoisson3d.h"
+#include "mixedpoisson.h"
+#include "pzbndcond.h"
+#include "TPZSSpStructMatrix.h"
+#include "pzskylstrmatrix.h"
+#include "TPZSkylineNSymStructMatrix.h"
+#include "TPZHybridizeHDiv.h"
+#include "pzbuildmultiphysicsmesh.h"
+
+using namespace std;
+
+int main(){
+    
+std::cout<<"Listo"<<std::endl;
+std::cout<<"Funciono"<<std::endl;
+
+}
