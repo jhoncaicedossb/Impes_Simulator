758830ed6d66df2cc94da0c9f79d7fdc987a8237
Kevin_Caicedo <kevinssb@hotmail.com>
2019-06-17 07:48:09 -0500

    update

--- IMPES_TEST/ImpesMain.cpp
+++ IMPES_TEST/ImpesMain.cpp
@@ -61,12 +61,70 @@
 #include "TPZSkylineNSymStructMatrix.h"
 #include "TPZHybridizeHDiv.h"
 #include "pzbuildmultiphysicsmesh.h"
+#include "TPZGmshReader.h"
+#include "TPZMixedDarcyFlow.h"
+#include "pzfmatrix.h"
+#include "pzskylstrmatrix.h"
+
+TPZCompMesh *GenerateCompMesh(TPZGeoMesh *gmesh);
 
 using namespace std;
 
 int main(){
+
+    //Leer la malla de gmsh
+TPZGmshReader read;
+read.SetFormatVersion("3");
+TPZGeoMesh *gmesh;
+// crea malla geometrica
+gmesh = read.GeometricGmshMesh("malla1.msh");
+std::ofstream archivo("malla_comp.vtk");
+TPZVTKGeoMesh::PrintGMeshVTK(gmesh, archivo);
+
+//crea malla computacional
+TPZCompMesh *cmesh = GenerateCompMesh(gmesh);
+TPZVTKGeoMesh::PrintCMeshVTK(cmesh,archivo);
+
+//crear analysis 
+TPZAnalysis *an = new TPZAnalysis(cmesh, true);
+std::cout<<"OK"<<std::endl;
+TPZSkylineStructMatrix matrix(cmesh);
+matrix.SetNumThreads(4);
+TPZStepSolver<STATE> step;
+step.SetDirect(ECholesky);
+an->SetSolver(step);
+
+an->SetStructuralMatrix(matrix);
+
+an->Solve();
+
+return 0;
+}
+
+TPZCompMesh *GenerateCompMesh(TPZGeoMesh *gmesh){
+    TPZCompMesh *cmesh = new TPZCompMesh(gmesh);
+    TPZMixedDarcyFlow *material = new TPZMixedDarcyFlow(4,2);
+    material->SetPermeability(10);
+    cmesh->InsertMaterialObject(material);
+    //D =0
+    //N =2
+    int dimension=2;
+    TPZFMatrix<STATE> val1(dimension,dimension,0.0), val2(dimension,1,0.0);
+    val2(0,0)=100;
+    TPZMaterial *bc_inlet = material->CreateBC(material,1,0,val1,val2);
+    cmesh->InsertMaterialObject(bc_inlet);
+    val2(0,0)=0;
+    TPZMaterial *bc_outlet = material->CreateBC(material,2,0,val1,val2);
+    cmesh->InsertMaterialObject(bc_outlet);
     
-std::cout<<"Listo"<<std::endl;
-std::cout<<"Funciono"<<std::endl;
+    val2(0,0)=0;
+    TPZMaterial *bc_impervious = material->CreateBC(material,3,1,val1,val2);
+    cmesh->InsertMaterialObject(bc_impervious);
+    
+    cmesh->SetAllCreateFunctionsContinuous();
+    cmesh->SetDefaultOrder(1);
+
+    cmesh->AutoBuild();
+
 
 }
